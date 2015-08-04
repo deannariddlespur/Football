82e71580feb355b6b5d7d5ad86445259b78e9be1
DeAnna Lynn Riddlespur <webdevdea@gmail.com>
2015-08-04 15:40:47 -0500

    adding rest framework to the project

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'football',
+    'rest_framework',
 
 )
 
@@ -102,3 +103,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+   'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+   'PAGE_SIZE': 10
+}
