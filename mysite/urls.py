fd15129bee5c2779093e112a13d8287a74cb6ad9
DeAnna Lynn Riddlespur <deannariddlespur@users.noreply.github.com>
2015-08-04 21:55:10 -0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""django_class URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from soccer import urls as soccer_urls
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^soccer-api/', include(soccer_urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
 ]
